a3491607acfe03fed4dab6315af970f691b3d9f4
DFKorn <d1man2192@mail.ru>
2022-04-06 20:54:13 +0300

    add hello

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello git! This is my react app, hope you like it.
         </p>
         <a
           className="App-link"
